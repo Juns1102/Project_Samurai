5b7297e6a3af047ab728e38742aa1fd7c52a0f49
Juns1102 <wnstj1302@naver.com>
2024-10-11 03:36:37 +0900

    player update

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -1,18 +1,118 @@
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class PlayerAnimation : MonoBehaviour
 {
+    [SerializeField]
+    private int force;
+
+    private bool attack1;
+    private bool attack2;
+    private bool attack3;
+
+    private bool ready2;
+    private bool ready3;
+
     private Animator anim;
+    private Rigidbody2D rb;
 
-    private void Start() {
+    private void Awake() {
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        attack1 = false;
+        attack2 = false;
+        attack3 = false;
+        ready2 = false;
+        ready3 = false;
     }
 
-    private void SetAttack() {
-        gameObject.GetComponentInChildren<BoxCollider2D>().enabled = true;
+    public bool State1() {
+        if(!attack1 && !attack2 && !attack3) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
-    private void EndAttack() {
-        gameObject.GetComponentInChildren<BoxCollider2D>().enabled = false;
+    public bool State2() {
+        if (attack1 && !attack2 && !attack3) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public bool State3() {
+        if (!attack1 && attack2 && !attack3) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    private void Start1() {
+        attack1 = true;
+        attack2 = false;
+        attack3 = false;
+    }
+
+    private void Start2() {
+        attack1 = false;
+        attack2 = true;
+        attack3 = false;
+    }
+
+    private void Start3() {
+        attack1 = false;
+        attack2 = false;
+        attack3 = true;
+    }
+
+    private void End1() {
+        attack1 = false;
+        attack2 = false;
+        attack3 = false;
+        if (ready2) {
+            anim.SetTrigger("Attack2");
+            ready2 = false;
+        }
+    }
+
+    private void End2() {
+        attack1 = false;
+        attack2 = false;
+        attack3 = false;
+        if (ready3) {
+            anim.SetTrigger("Attack3");
+            ready3 = false;
+        }
+    }
+
+    private void End3() {
+        attack1 = false;
+        attack2 = false;
+        attack3 = false;
+    }
+
+    public void Ready2() {
+        ready2 = true;
+    }
+
+    public void Ready3() {
+        ready3 = true;
+    }
+
+    private void AttackForce() {
+        if (transform.rotation.y == 0) {
+            rb.linearVelocity = Vector2.zero;
+            rb.AddForceX(force, ForceMode2D.Force);
+        }
+        else {
+            rb.linearVelocity = Vector2.zero;
+            rb.AddForceX(-force, ForceMode2D.Force);
+        }
     }
 }
--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -2,27 +2,25 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    Animator anim;
+    private Animator anim;
+    private PlayerAnimation pAnim;
+    private Rigidbody2D rb;
 
     private void Awake() {
         anim = GetComponent<Animator>();
+        pAnim = GetComponent<PlayerAnimation>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnAttack() {
-        if(!anim.GetCurrentAnimatorStateInfo(0).IsName("Attack1") && 
-            !anim.GetCurrentAnimatorStateInfo(0).IsName("Attack2") && 
-            !anim.GetCurrentAnimatorStateInfo(0).IsName("Attack3")) {
+        if(pAnim.State1()) {
             anim.SetTrigger("Attack1");
         }
-        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack1") &&
-            !anim.GetCurrentAnimatorStateInfo(0).IsName("Attack2") &&
-            !anim.GetCurrentAnimatorStateInfo(0).IsName("Attack3")) {
-            anim.SetTrigger("Attack2");
+        else if (pAnim.State2()) {
+            pAnim.Ready2();
         }
-        else if(!anim.GetCurrentAnimatorStateInfo(0).IsName("Attack1") &&
-            anim.GetCurrentAnimatorStateInfo(0).IsName("Attack2") &&
-            !anim.GetCurrentAnimatorStateInfo(0).IsName("Attack3")) {
-            anim.SetTrigger("Attack3");
+        else if(pAnim.State3()) {
+            pAnim.Ready3();
         }
     }
 }
