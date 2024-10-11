08f245c36e700f657a7d4f8c8a6ef87c228035f6
Juns1102 <wnstj1302@naver.com>
2024-10-12 03:22:46 +0900

    movement update

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -4,9 +4,15 @@ using UnityEngine.InputSystem;
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField]
+    float startSpeed;
+    [SerializeField]
+    float maxSpeed;
+    [SerializeField] 
     float speed;
     [SerializeField]
     float jumpPower;
+    [SerializeField]
+    bool change;
 
     public float inputValue;
 
@@ -21,10 +27,16 @@ public class PlayerMove : MonoBehaviour
     }
 
     private void FixedUpdate() {
+        if (inputValue == 0f) {
+            speed = startSpeed;
+        }
         if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Attack1") && 
             !anim.GetCurrentAnimatorStateInfo(0).IsName("Attack2") && 
             !anim.GetCurrentAnimatorStateInfo(0).IsName("Attack3")) {
             body.linearVelocityX = inputValue * speed;
+            if (speed < maxSpeed) {
+                speed += 0.5f;
+            }
         }
     }
 
@@ -45,6 +57,7 @@ public class PlayerMove : MonoBehaviour
 
     private void OnMove(InputValue value) {
         inputValue = value.Get<Vector2>().x;
+        
     }
 
     private void OnJump() {
